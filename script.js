2af80980d25d3072ce3be5bfa2d894e574122a0e
Palash Bajpai <palashbajpai@gmail.com>
2024-07-18 22:01:54 +0530

    changing animation

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 gsap.registerPlugin(ScrollTrigger) 
+var w =  window.screen.width;
 
 
 gsap.from('.logo-box', {
     scrollTrigger: '.logo-box', // start the animation when ".box" enters the viewport (once)
-    duration:1.2,
-    ease: 'power1.out',
-    visibility:'hidden',
+    duration:1,
+    ease: "bounce.out",
+    x:w,
     stagger:0.05
-});
-
-gsap.from('.logo-box', {
-    scrollTrigger: '.logo-box', // start the animation when ".box" enters the viewport (once)
-    duration:1.8,
-    ease: 'power1.out',
-    filter:'blur(5px)',
-    stagger:0.15
 });
 
 
