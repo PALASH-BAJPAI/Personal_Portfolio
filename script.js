0ff21758e4bc128ebc18e21fb0320090611edae4
Palash Bajpai <palashbajpai@gmail.com>
2024-07-18 22:05:20 +0530

    Fixing overflow issue

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var w =  window.screen.width;
 gsap.from('.logo-box', {
     scrollTrigger: '.logo-box', // start the animation when ".box" enters the viewport (once)
     duration:1,
+    display:'none',
     ease: "bounce.out",
     x:w,
     stagger:0.05
